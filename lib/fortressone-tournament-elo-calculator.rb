cb25c65a6641e8e5d6724ad5a2f9aecfa086a13f
Sheldon Johnson <shayolden@hotmail.com>
2019-01-30 02:27:01 +1100

    Add elo calculator with some results already

---
+++ lib/fortressone-tournament-elo-calculator.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'elo'
+require 'pry'
+
+Elo.configure do |config|
+  config.default_rating = 1000
+  config.pro_rating_boundry = 2400
+  config.starter_boundry = 30
+end
+
+teams = {}
+
+%w[sobar charming ragtag chiken penguin].each do |team|
+  teams[team] = Elo::Player.new
+end
+
+teams["sobar"].wins_from(teams["penguin"])
+teams["charming"].wins_from(teams["chiken"])
+teams["charming"].wins_from(teams["sobar"])
+teams["ragtag"].wins_from(teams["chiken"])
+teams["ragtag"].wins_from(teams["penguin"])
+
+results = teams.map do |name, elo|
+  {name => elo.rating}
+end
+
+puts results
